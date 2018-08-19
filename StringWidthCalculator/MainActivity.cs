019ef7a089d3de6ac7fed467635ade87db2a0ed6
Hiroaki Tomiyoshi <htomiyosi@gmail.com>
2018-08-19 15:29:15 +0900

    Update

---
+++ StringWidthCalculator/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace StringWidthCalculator
+{
+    [Activity(Label = "StringWidthCalculator", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        EditText EditString { get; set; }
+        EditText EditFontSize { get; set; }
+        Button CalculateButton { get; set; }
+        TextView ResultTextView { get; set; }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            EditString = FindViewById<EditText>(Resource.Id.editString);
+            EditFontSize = FindViewById<EditText>(Resource.Id.editFontSize);
+            CalculateButton = FindViewById<Button>(Resource.Id.calculate);
+            ResultTextView = FindViewById<TextView>(Resource.Id.result);
+
+            CalculateButton.Click += (sender, e) => {
+                var str = EditString.Text;
+                var fontSize = double.Parse(EditFontSize.Text);
+                var calculator = new StringWidthCalculator();
+                var widthSp = calculator.CalculateStringWidth(str, fontSize);
+                var widthDp = calculator.CalculateStringWidthDP(str, fontSize);
+                ResultTextView.Text = $"width(SP): {widthSp}\nwidth(DP): {widthDp}";
+            };
+        }
+    }
+}
+
---
+++ StringWidthCalculator/StringWidthCalculator.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Content.Res;
+using Android.Util;
+using Android.Widget;
+
+namespace StringWidthCalculator
+{
+    public class StringWidthCalculator
+    {
+
+        public double CalculateStringWidth(string str, double fontSize)
+        {
+            var textView = new TextView(Application.Context);
+            textView.SetTextSize(ComplexUnitType.Sp, (float)fontSize);
+            var widthOfPixel = textView.Paint.MeasureText(str);
+            return widthOfPixel / Resources.System.DisplayMetrics.Density;
+        }
+
+        public double CalculateStringWidthDP(string str, double fontSize)
+        {
+            var textView = new TextView(Application.Context);
+            textView.SetTextSize(ComplexUnitType.Dip, (float)fontSize);
+            var widthOfPixel = textView.Paint.MeasureText(str);
+            return widthOfPixel / Resources.System.DisplayMetrics.Density;
+        }
+    }
+}
